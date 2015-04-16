c3054679fc776f98551176c269f3fc73c031641d
VaryagUbisoft <Anton.Chibisov.Ubisoft@mail.ru>
2015-04-16 15:12:37 +0300

    Отключен третий поток.

--- Source.cpp
+++ Source.cpp
@@ -17,12 +17,16 @@ public:
 
     ThreadInfo()
     {
+#ifdef SYNCHRONIZE_THREADS
         InitializeCriticalSection(&m_criticalSection);
+#endif
     }
 
     ~ThreadInfo()
     {
+#ifdef SYNCHRONIZE_THREADS
         DeleteCriticalSection(&m_criticalSection);
+#endif
     }
 
     void SetThreadId(int threadId)
@@ -76,7 +80,10 @@ public:
 
 private:
 
+#ifdef SYNCHRONIZE_THREADS
     CRITICAL_SECTION m_criticalSection;
+#endif
+
     int   m_threadId;
     char* m_threadName;
     
@@ -105,6 +112,11 @@ public:
         m_threadInfo->SetThreadName(m_threadName);
 
         cout << "Thread " << m_threadInfo->GetThreadThreadName() << " is running" << " with Id " << m_threadInfo->GetThreadId() << "\n\n";
+    }
+
+    ~WriterThread()
+    {
+        cout << "Thread " << m_threadInfo->GetThreadThreadName() << " with Id " << m_threadInfo->GetThreadId() << "terminated" <<"\n\n";
     }
 
 private:
@@ -169,30 +181,33 @@ void main()
     uintptr_t t2Id = 0;
     HANDLE t2H = (HANDLE)_beginthreadex(0, 0, &WriterThread::StaticEntryPoint, t2, CREATE_SUSPENDED, &t2Id);
 
-    
+    /*
     ReaderThread* t3 = new ReaderThread(threadInfo, {t1H, t2H});
     uintptr_t t3Id = 0;
     HANDLE t3H = (HANDLE)_beginthreadex(0, 0, &ReaderThread::StaticEntryPoint, t3, CREATE_SUSPENDED, &t3Id);
-    
+    ResumeThread(t3H);
+    WaitForSingleObject(t3H, INFINITE);
+    */
 
     // Let's start our suspended threads
 
     ResumeThread(t1H);
     ResumeThread(t2H);    
-    ResumeThread(t3H);
+    
+    Sleep(100);
 
     // In order for the main thread not to terminate causing termination of a process before the other two threads started
     // we force it to wait upon t1 and t2
     
-    WaitForSingleObject(t1H, INFINITE);
-    WaitForSingleObject(t2H, INFINITE);
-    WaitForSingleObject(t3H, INFINITE);
+    //WaitForSingleObject(t1H, INFINITE);
+   // WaitForSingleObject(t2H, INFINITE);
+    
 
     // _beginthreadex doesn't automatically close thread's handle after terminating
     // It must be done manually
     CloseHandle(t1H);
     CloseHandle(t2H);
-    CloseHandle(t3H);
+    //CloseHandle(t3H);
 
 }
 

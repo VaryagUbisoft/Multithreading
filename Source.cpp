b61e04e1c6791fe1075f8682a5b940850e201952
VaryagUbisoft <Anton.Chibisov.Ubisoft@mail.ru>
2015-06-07 23:08:22 +0300

    Refactored

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,7 @@
 #include <Windows.h>
 #include <process.h>
 #include <vector>
+#include <ctime>
 
 
 using namespace std;
@@ -10,73 +11,50 @@ using namespace std;
 
 #define SYNCHRONIZE_THREADS
 
+// Shared data
 
-class ThreadInfo
+class PolarVec
 {
 public:
 
-    ThreadInfo()
+    PolarVec() : A(0), phi(0)
     {
 #ifdef SYNCHRONIZE_THREADS
         InitializeCriticalSection(&m_criticalSection);
 #endif
     }
 
-    ~ThreadInfo()
+    ~PolarVec()
     {
 #ifdef SYNCHRONIZE_THREADS
         DeleteCriticalSection(&m_criticalSection);
 #endif
     }
+    
 
-    void SetThreadId(int threadId)
+    void SetVec(float amplitude, float angle)
     {
 #ifdef SYNCHRONIZE_THREADS
         EnterCriticalSection(&m_criticalSection);
 #endif
-        m_threadId = threadId;
-#ifdef SYNCHRONIZE_THREADS
-        LeaveCriticalSection(&m_criticalSection);
-#endif
-
-    }
-
-    int GetThreadId()
-    {
-#ifdef SYNCHRONIZE_THREADS
-        EnterCriticalSection(&m_criticalSection);
-#endif
-        return m_threadId;
+        A = amplitude;
+        phi = angle;
 #ifdef SYNCHRONIZE_THREADS
         LeaveCriticalSection(&m_criticalSection);
 #endif
     }
 
-    void SetThreadName(char* threadName)
+    void GetVec(float& amplitude, float& angle)
     {
 #ifdef SYNCHRONIZE_THREADS
         EnterCriticalSection(&m_criticalSection);
 #endif
-        delete m_threadName;
-            m_threadName = new char[strlen(threadName)];
-        m_threadName = threadName;
-#ifdef SYNCHRONIZE_THREADS
-        LeaveCriticalSection(&m_criticalSection);
-#endif
-
-    }
-
-    char* GetThreadThreadName()
-    {
-#ifdef SYNCHRONIZE_THREADS
-        EnterCriticalSection(&m_criticalSection);
-#endif
-        return m_threadName;
+        amplitude = A;
+        angle = phi;
 #ifdef SYNCHRONIZE_THREADS
         LeaveCriticalSection(&m_criticalSection);
 #endif
     }
-
 
 private:
 
@@ -84,136 +62,85 @@ private:
     CRITICAL_SECTION m_criticalSection;
 #endif
 
-    int   m_threadId;
-    char* m_threadName;
-    
+
+    float A;
+    float phi;
+
 };
 
 
+// Generic thread for read/write purposes
 
-class WriterThread
+class ThreadRW
 {
 public:
 
-    WriterThread(char* threadName, int threadId, ThreadInfo* threadInfo) : m_threadName(threadName), m_threadId(threadId), m_threadInfo(threadInfo){}
+    ThreadRW(PolarVec* sharedData, bool isWriterThread) : isWriter(isWriterThread), v(sharedData){}
 
-    
-    static unsigned __stdcall StaticEntryPoint(void* pObj)
+    static unsigned __stdcall EntryPointTrigger(void* arg)
     {
-        WriterThread* thread = static_cast<WriterThread*>(pObj);
-        thread->EntryPoint();
-
+        ThreadRW* pThreadRW = static_cast<ThreadRW*>(arg);
+        pThreadRW->EntryPoint();
         return 1;
     }
 
     void EntryPoint()
     {
-        m_threadInfo->SetThreadId(m_threadId);
-        m_threadInfo->SetThreadName(m_threadName);
-
-        cout << "Thread " << m_threadInfo->GetThreadThreadName() << " is running" << " with Id " << m_threadInfo->GetThreadId() << "\n\n";
-    }
-
-    ~WriterThread()
-    {
-        cout << "Thread " << m_threadInfo->GetThreadThreadName() << " with Id " << m_threadInfo->GetThreadId() << "terminated" <<"\n\n";
-    }
-
-private:
-
-    ThreadInfo* m_threadInfo;
-
-    int m_threadId;
-    char* m_threadName;
-
-};
-
-
-class ReaderThread
-{
-public:
-
-    ReaderThread(ThreadInfo* threadInfo) : m_threadInfo(threadInfo) {}
-    ReaderThread(ThreadInfo* threadInfo, const std::vector<HANDLE> threadsToWaitOn) : m_threadInfo(threadInfo), m_threadsToWaitOn(threadsToWaitOn) {}
-
-    static unsigned __stdcall StaticEntryPoint(void* pObj)
-    {
-        ReaderThread* thread = static_cast<ReaderThread*>(pObj);
-        thread->EntryPoint();
-
-        return 1;
-    }
-
-    void EntryPoint()
-    {
-        if (!m_threadsToWaitOn.empty())
+        if (isWriter)
         {
-            for (auto it_threadHndl = m_threadsToWaitOn.begin(); it_threadHndl != m_threadsToWaitOn.end(); ++it_threadHndl)
+            cout << "Now in WRITER THREAD\n\n";
+            for (int i = 0; i < 200000000; ++i)
+                v->SetVec(i, i);
+        }
+        else
+        {
+            cout << "Now in READER THREAD\n\n";
+            float a = -1;
+            float phi = -1;
+            for (int i = 0; i < 200; ++i)
             {
-                WaitForSingleObject(*it_threadHndl, INFINITE);
-                m_threadsToWaitOn.erase(it_threadHndl);
+                v->GetVec(a, phi);
+                cout << "Amp: " << a << " Angle: " << phi << "\n\n";
+                Sleep(10);
             }
         }
-
-
-        cout << "Thread " << m_threadInfo->GetThreadThreadName() << " is running" << " with Id " << m_threadInfo->GetThreadId() << "\n\n";
     }
 
 private:
 
-    ThreadInfo* m_threadInfo;
-    std::vector<HANDLE> m_threadsToWaitOn;
-
+    bool isWriter;
+    PolarVec* v;
 };
-
-
-
 
 void main()
 {
-    ThreadInfo* threadInfo = new ThreadInfo;
+    PolarVec* pv = new PolarVec();
 
-    WriterThread* t1 = new WriterThread("WriterThread1", 111, threadInfo);
-    uintptr_t t1Id = 0;
-    HANDLE t1H = (HANDLE)_beginthreadex(0, 0, &WriterThread::StaticEntryPoint, t1, CREATE_SUSPENDED, &t1Id);
+    ThreadRW* tw = new ThreadRW(pv, true);
+    ThreadRW* tr = new ThreadRW(pv, false);
 
-    WriterThread* t2 = new WriterThread("WriterThread2", 222, threadInfo);
-    uintptr_t t2Id = 0;
-    HANDLE t2H = (HANDLE)_beginthreadex(0, 0, &WriterThread::StaticEntryPoint, t2, CREATE_SUSPENDED, &t2Id);
+    unsigned twID, trID;
 
-    /*
-    ReaderThread* t3 = new ReaderThread(threadInfo, {t1H, t2H});
-    uintptr_t t3Id = 0;
-    HANDLE t3H = (HANDLE)_beginthreadex(0, 0, &ReaderThread::StaticEntryPoint, t3, CREATE_SUSPENDED, &t3Id);
-    ResumeThread(t3H);
-    WaitForSingleObject(t3H, INFINITE);
-    */
+    HANDLE twH, trH;
 
-    // Let's start our suspended threads
+    twH = (HANDLE)_beginthreadex(NULL, 0, ThreadRW::EntryPointTrigger, tw, CREATE_SUSPENDED, &twID);
+    trH = (HANDLE)_beginthreadex(NULL, 0, ThreadRW::EntryPointTrigger, tr, CREATE_SUSPENDED, &trID);
 
-    ResumeThread(t1H);
-    ResumeThread(t2H);    
-    
+    ResumeThread(twH);
+    ResumeThread(trH);
+
     Sleep(100);
 
-    // In order for the main thread not to terminate causing termination of a process before the other two threads started
-    // we force it to wait upon t1 and t2
-    
-    //WaitForSingleObject(t1H, INFINITE);
-   // WaitForSingleObject(t2H, INFINITE);
-    
+    WaitForSingleObject(twH, INFINITE);
+    WaitForSingleObject(trH, INFINITE);
 
-    // _beginthreadex doesn't automatically close thread's handle after terminating
-    // It must be done manually
-    CloseHandle(t1H);
-    CloseHandle(t2H);
-    //CloseHandle(t3H);
+    CloseHandle(twH);
+    CloseHandle(trH);
 
+    delete tw;
+    tw = NULL;
+
+    delete tr;
+    tr = NULL;
 }
-
-
-
-
-
-
 
